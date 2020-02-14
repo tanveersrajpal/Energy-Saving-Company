b88445f17c43fa9db53ca32adbefd9830fdc0cbd
Tanveer Rajpal <56073437+tanveersrajpal@users.noreply.github.com>
2020-02-14 20:30:47 +1300

    Add files via upload

---
+++ Models/Courses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Techofab_Website.Models
+{
+    public class Courses
+    {
+        public string MTA { get; set; }
+        public string MCSA { get; set; }
+    }
+}
---
+++ Models/CustomerDetails.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Techofab_Website.Models
+{
+    public class CustomerDetails
+    {
+        [Required(ErrorMessage ="First name is required")]
+        [Display(Name ="First name")]
+        [StringLength(10, ErrorMessage =("The name should not exceed 10 characters"))]
+        public string Fname { get; set; }
+
+
+        [Required(ErrorMessage = "First name is required")]
+        [Display(Name = "Last name")]
+        public string Lname { get; set; }
+
+
+        [Required(ErrorMessage = "First name is required")]
+        [Display(Name = "Contact")]
+        public int Contact { get; set; }
+
+
+        [Required(ErrorMessage ="Please enter your Email address")]
+        [Display(Name="Email Id")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "First name is required")]
+        [Display(Name = "Course interested")]
+
+        
+        public string Courseinterested { get; set; }
+        [Required(ErrorMessage = "First name is required")]
+        [Display(Name = "Contact preference")]
+        public string Contactpreference { get; set; }
+
+    }
+}
